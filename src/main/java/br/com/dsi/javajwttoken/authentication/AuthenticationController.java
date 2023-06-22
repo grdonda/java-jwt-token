ec33ad3e9a66bb780f9fd9d5a69e4fe892d68f9a
Giovanni Donda <grdonda@gmail.com>
2023-06-21 22:30:03 -0300

    Adicionado os endpoints para gerar o token e validar o token jwt

---
+++ src/main/java/br/com/dsi/javajwttoken/authentication/AuthenticationController.java
@@ -0,0 +1,65 @@
+package br.com.dsi.javajwttoken.authentication;
+
+import br.com.dsi.javajwttoken.authentication.dto.TokenJwtDTO;
+import br.com.dsi.javajwttoken.authentication.dto.TokenRequestDTO;
+import br.com.dsi.javajwttoken.authentication.dto.TokenValidateDTO;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    public static final String SECRET = "secret";
+    public static final String ISSUER = "issuer";
+
+    @PostMapping("/token")
+    public ResponseEntity<TokenJwtDTO> getToken(@RequestBody @Valid TokenRequestDTO data) {
+
+        String username = data.username();
+
+        try {
+            Algorithm algoritimo = getAlgorithm();
+            String token = JWT.create()
+                    .withSubject(username)
+                    .withIssuer(ISSUER)
+                    .sign(algoritimo);
+
+            return ResponseEntity.ok(new TokenJwtDTO(token));
+        } catch (JWTCreationException exception) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/token/validate")
+    public ResponseEntity<TokenJwtDTO> validateToken(@RequestBody @Valid TokenValidateDTO token) {
+
+        DecodedJWT decodedJWT;
+        try {
+
+            Algorithm algorithm = getAlgorithm();
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build();
+
+            decodedJWT = verifier.verify(token.token());
+            return ResponseEntity.ok(new TokenJwtDTO(decodedJWT.getToken()));
+        } catch (JWTVerificationException exception) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    private Algorithm getAlgorithm() {
+        return Algorithm.HMAC256(SECRET);
+    }
+}
---
+++ src/main/java/br/com/dsi/javajwttoken/authentication/dto/TokenValidateDTO.java
@@ -0,0 +1,4 @@
+package br.com.dsi.javajwttoken.authentication.dto;
+
+public record TokenValidateDTO(String token) {
+}
